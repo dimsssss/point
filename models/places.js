e6d73872f29791ac7b68e7bcb81e036feecfd3d9
dimsssss <caruen@hanmail.net>
2022-06-16 09:22:03 +0900

    feat: model 추가

---
+++ models/places.js
@@ -0,0 +1,50 @@
+const { v4 : uuidv4 } = require('uuid');
+const { format } = require('date-fns');
+
+module.exports = (sequelize, DataTypes) => {
+    const places = sequelize.define('places', {
+        userId: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        isBonus: {
+            type: DataTypes.TINYINT,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            get() {
+                const dateText = this.getDataValue('createdAt');
+                return format(new Date(dateText), 'yyyy-MM-dd HH:mm:ss');
+            }
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            get() {
+                const dateText = this.getDataValue('updatedAt');
+                return format(new Date(dateText), 'yyyy-MM-dd HH:mm:ss');
+            }
+        }
+    }, {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+        freezeTableName: true,
+    });
+
+    places.addPlace = async (data) => {
+        const placeId = uuidv4();
+        const result = await places.create({...data, placeId}).catch((err) => {
+            return err;
+        });
+        return result;
+    }
+
+    return places;
+}
