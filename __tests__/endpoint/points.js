828f29813ec4033172b43a6d19d0f38b5628e12d
dimsssss <caruen@hanmail.net>
2022-06-22 17:18:29 +0900

    refactor: 하드코딩된 데이터 제거

--- __tests__/endpoint/points.js
+++ __tests__/endpoint/points.js
@@ -5,7 +5,27 @@ const {v4:uuid} = require('uuid')
 
 app.set('db', db);
 
+const createFakeData = () => {
+    return {
+        type: "REVIEW",
+        action: "ADD",
+        content: "굿",
+        userId: uuid(),
+        placeId: uuid(),
+        attachedPhotoIds: [
+            uuid()
+        ],
+        reviewId: uuid()
+    }
+}
+
 describe('points endpoint 테스트입니다', function () {
+
+    afterEach(async () => {
+        await db.points.destroy({where: {}});
+        await db.pointsHistory.destroy({where: {}});
+    })
+
     it('사용자의 포인트가 정보가 없을 시 null 값 반환', async function () {
         const userId = uuid();
         const response = await request(app).get(`/v1/points/${userId}`);
@@ -16,26 +36,16 @@ describe('points endpoint 테스트입니다', function () {
     });
 
     it('사용자의 포인트를 정보를 조회한다(userId, totalPoint)', async function () {
-        const data = {
-            type: "REVIEW",
-            action: "ADD",
-            content: "굿",
-            userId: "3ede0ef2-92b7-4817-a5f3-0c575361f745",
-            placeId: "a8a3f646-fc48-4944-880e-ad10b92c3e24",
-            attachedPhotoIds: [
-                "1c4d1930-1765-46c4-92ae-cc0bccfa15fe"
-            ],
-            reviewId: "1ad93103-ebe5-4c43-b952-7419d00c41ef"
-        }
+        const fakeData = createFakeData();
         // given
         const {addNewPointForWritingReview} = require('../../services/points')
-        await addNewPointForWritingReview(db, data);
+        await addNewPointForWritingReview(db, fakeData);
 
         //when
-        const result = await request(app).get(`/v1/points/${data.userId}`);
+        const result = await request(app).get(`/v1/points/${fakeData.userId}`);
 
         expect(result.status).toEqual(200);
-        expect(result.body.userId).toEqual(data.userId);
+        expect(result.body.userId).toEqual(fakeData.userId);
         expect(result.body.totalPoint).toEqual('3');
     });
 })
--- __tests__/intergration/points.js
+++ __tests__/intergration/points.js
@@ -1,98 +1,84 @@
 const db = require('../../models/index');
+const {v4:uuid} = require('uuid');
 const {addNewPointForWritingReview, modifyPointByEditingReview, deletePointForDeleteReview} = require('../../services/points');
 
-
-const addPointTest = async function (data) {
-    await addNewPointForWritingReview(db, data);
-    const result = await db.points.findAll({
-        where: {
-            userId: data.userId,
-            placeId: data.placeId,
-            reviewId: data.reviewId
-        }, plain: true
-    });
-    const historyResult = await db.pointsHistory.findOne({where:{userId: data.userId, reviewId: data.reviewId}, plain: true});
-
-    expect(result.userId).toEqual(data.userId);
-    expect(result.placeId).toEqual(data.placeId);
-    expect(result.reviewId).toEqual(data.reviewId);
-
-    expect(historyResult.userId).toEqual(data.userId);
-    expect(historyResult.reviewId).toEqual(data.reviewId);
-}
-
-
-describe('points 통합테스트입니다.', () => {
-    afterEach(async () => {
-        await db.points.destroy({where: {}});
-        await db.pointsHistory.destroy({where: {}});
-    })
-
-    const firstEvent = {
-        type: "REVIEW",
-        action: "ADD",
-        content: "첫리뷰",
-        userId: "a1f99025-28b5-42f8-a284-f4aedc79d785",
-        placeId: "e88c6f49-894e-45df-b24a-fb6e2ada2546",
-        attachedPhotoIds: [
-            "7d5cdf33-d912-47e3-9fc0-b9ac276ed8cf"
-        ],
-        reviewId: "1ad93103-ebe5-4c43-b952-7419d00c41ef"
-    }
-
-    const secondEvent = {
+const createFakeData = () => {
+    return {
         type: "REVIEW",
         action: "ADD",
         content: "굿",
-        userId: "a1f99025-28b5-42f8-a284-f4aedc79d785",
-        placeId: "a8a3f646-fc48-4944-880e-ad10b92c3e24",
+        userId: uuid(),
+        placeId: uuid(),
         attachedPhotoIds: [
-            "1c4d1930-1765-46c4-92ae-cc0bccfa15fe"
+            uuid()
         ],
-        reviewId: "e515ece7-a3ce-478d-bc3a-fa363e2e36ce"
+        reviewId: uuid()
     }
+}
 
-    it('리뷰를 작성하였을 때 보상 포인트를 얻는다', addPointTest.bind(this, firstEvent));
+describe('points 통합테스트입니다.', () => {
+    it('리뷰를 작성하였을 때 보상 포인트를 얻는다', async () => {
+        const fakeData = createFakeData();
+        await addNewPointForWritingReview(db, fakeData);
+        const result = await db.points.findAll({
+            where: {
+                userId: fakeData.userId,
+                placeId: fakeData.placeId,
+                reviewId: fakeData.reviewId
+            }, plain: true
+        });
+        const historyResult = await db.pointsHistory.findOne({where:{userId: fakeData.userId, reviewId: fakeData.reviewId}, plain: true});
+
+        expect(result.userId).toEqual(fakeData.userId);
+        expect(result.placeId).toEqual(fakeData.placeId);
+        expect(result.reviewId).toEqual(fakeData.reviewId);
+
+        expect(historyResult.userId).toEqual(fakeData.userId);
+        expect(historyResult.reviewId).toEqual(fakeData.reviewId);
+    });
 
     it('사용자들은 하나의 장소에 하나의 리뷰만 가능하다', async function () {
         const {duplicatedPointException} = require('../../exceptions/index')
-
-        await addNewPointForWritingReview(db, firstEvent);
+        const fakeData = createFakeData();
+        await addNewPointForWritingReview(db, fakeData);
         expect(async () => {
-            await addNewPointForWritingReview(db, firstEvent)
+            await addNewPointForWritingReview(db, fakeData)
         }).rejects.toThrow(new duplicatedPointException());
     });
 
     it('유저가 다른 장소로 리뷰를 남길 수 있다', async function () {
         const {getUserPoint} = require('../../services/points');
         // 생성된 포인트 2개 확인
-        await addPointTest(firstEvent);
-        await addPointTest(secondEvent);
+        const oneFakeData = createFakeData();
+        const secondFakeData = createFakeData();
+        secondFakeData.userId = oneFakeData.userId;
+        await addNewPointForWritingReview(db, oneFakeData);
+        await addNewPointForWritingReview(db, secondFakeData);
         // 포인트 합산 결과 확인
-        const pointInformation = await getUserPoint(db, firstEvent);
-        expect(pointInformation.userId).toEqual(firstEvent.userId);
+        const pointInformation = await getUserPoint(db, oneFakeData);
+        expect(pointInformation.userId).toEqual(oneFakeData.userId);
         expect(pointInformation.totalPoint).toEqual('6')
     });
 
     it('보상 포인트는 변경될 수 있다', async function () {
-        await addPointTest(firstEvent);
-        const modifiedPhotoIdsEvent = {...firstEvent};
-        modifiedPhotoIdsEvent.attachedPhotoIds = [];
+        const fakeData = createFakeData();
+        await addNewPointForWritingReview(db, fakeData);
+        const prevPoint = await db.points.findOne({where: {userId: fakeData.userId, placeId: fakeData.placeId, reviewId: fakeData.reviewId}, raw: true});
 
-        const prevPoint = await db.points.findOne({where: {userId: firstEvent.userId, placeId: firstEvent.placeId, reviewId: firstEvent.reviewId}, plain: true});
+        fakeData.attachedPhotoIds = [];
 
-        await modifyPointByEditingReview(db, modifiedPhotoIdsEvent);
+        await modifyPointByEditingReview(db, fakeData);
 
-        const modifiedPoint = await db.points.findOne({where: {userId: firstEvent.userId, placeId: firstEvent.placeId, reviewId: firstEvent.reviewId}, plain: true});
-
+        const modifiedPoint = await db.points.findOne({where: {userId: fakeData.userId, placeId: fakeData.placeId, reviewId: fakeData.reviewId}, raw: true});
+        console.log(modifiedPoint)
         expect(prevPoint.point).not.toEqual(modifiedPoint.point);
-
     });
 
     it('리뷰를 수정하고 포인트의 변동이 없다면 히스토리는 쌓이지 않는다', async function () {
-        await addPointTest(firstEvent);
+        const fakeData = createFakeData();
+        await addNewPointForWritingReview(db, fakeData);
 
-        const modEvent = {...firstEvent};
+        const modEvent = {...fakeData};
         modEvent.action = 'MOD';
 
         await modifyPointByEditingReview(db, modEvent);
@@ -103,25 +89,29 @@ describe('points 통합테스트입니다.', () => {
     });
 
     it('존재하지 않는 포인트를 회수할 때 에러를 발생시킨다.', async function () {
-        const {notFoundPointException} = require('../../exceptions/index')
+        const {notFoundPointException} = require('../../exceptions/index');
+        const fakeData = createFakeData();
         expect(async () => {
-            await deletePointForDeleteReview(db, firstEvent);
+            fakeData.action = 'DELETE';
+            await deletePointForDeleteReview(db, fakeData);
         }).rejects.toThrow(new notFoundPointException());
     });
 
     it('리뷰를 삭제하면 보상 포인트를 회수할 수 있다.', async function () {
-        await addPointTest(firstEvent);
+        const fakeData = createFakeData();
+        await addNewPointForWritingReview(db, fakeData);
 
         const point = await db.sequelize.transaction(async (t) => {
-            return await db.points.findTotalPointByUserId(firstEvent.userId, t);
+            return await db.points.findTotalPointByUserId(fakeData.userId, t);
         });
 
         expect(point.totalPoint).toEqual('3');
 
-        await deletePointForDeleteReview(db, firstEvent);
+        fakeData.action = 'DELETE'
+        await deletePointForDeleteReview(db, fakeData);
 
         const prevPoint = await db.sequelize.transaction(async (t) => {
-            return await db.points.findPointByReviewId(firstEvent.reviewId, t);
+            return await db.points.findPointByReviewId(fakeData.reviewId, t);
         });
 
         expect(prevPoint).toEqual(null);
